0fd24c7200240e8cb6a54fb55a18e5fd89fa627a
BraydenGreen <brayden.p.green@gmail.com>
2015-10-07 17:10:58 -0600

    WIP: adds controllers

--- chore_hero/app/controllers/chores_controller.rb
+++ chore_hero/app/controllers/chores_controller.rb
@@ -1,19 +1,54 @@
-class ChoresController < ApplicationController
-  def index
-    @chores = Chore.all
-  end
-
-  def new
-    @chore = chore.new
-  end
-
-  def create
-    @chore = Chore.find
+class BadgesController < ApplicationController
+  
+  before_action :find_badge, only: [:show, :edit, :update, :destroy]
+  
+  def show
   end
 
   def edit
   end
 
-  def show
+  def update
+    if @badge.update(badge_params)
+      redirect_to badge_path(@badge.id)
+    else
+      render 'edit'
+    end
   end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+      if @badge.save
+        redirect_to badge_path(@badge.id)
+      else
+        render 'new'
+      end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to badge_path(current_user)
+  end
+
+  def vote
+    @badge = Badge.find(params[:id])
+    @user = current_user
+    @badge.liked_by @user
+    redirect_to users_path
+  end
+
+  private
+
+     def badge_params
+       params.require(:badge).permit(:user_id, :title, :description, :badge_multiplier)
+     end
+    
+     def find_badge
+       @badge = badge.find(params[:id])
+     end
+
 end
