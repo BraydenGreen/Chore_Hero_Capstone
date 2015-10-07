8cfac51ca77ebdf18aa109857e9acaf360a29b9e
BraydenGreen <brayden.p.green@gmail.com>
2015-10-07 16:29:11 -0600

    WIP: adds controllers

--- chore_hero/app/controllers/children_controller.rb
+++ chore_hero/app/controllers/children_controller.rb
@@ -1,4 +1,5 @@
 class ChildrenController < ApplicationController
   def index
+    @children = Child.all
   end
 end
--- chore_hero/app/controllers/chores_controller.rb
+++ chore_hero/app/controllers/chores_controller.rb
@@ -1,11 +1,14 @@
 class ChoresController < ApplicationController
   def index
+    @chores = Chore.all
   end
 
   def new
+    @chore = chore.new
   end
 
   def create
+    @chore = Chore.find
   end
 
   def edit
