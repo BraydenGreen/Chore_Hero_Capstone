e3ac061fa449b3c2155623d297513f5a9cefff66
stuckiest <stuckiest@gmail.com>
2015-10-08 17:32:17 -0600

    add parent show page

--- chore_hero/app/controllers/parent/registrations_controller.rb
+++ chore_hero/app/controllers/parent/registrations_controller.rb
@@ -1,2 +1,13 @@
 class Parent::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    '/parent_show'
+  end
+
+  #this overrides the devise redirect to '/parent_show'
+  #this is a method that was written/provided by devise
+  def after_sign_in_path_for(resource)
+    '/parent_show'
+  end
 end
