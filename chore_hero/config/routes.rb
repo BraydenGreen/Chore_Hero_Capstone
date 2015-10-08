2fc8f22ca25b67db67da2a113c1f35d4318b3b6d
BraydenGreen <brayden.p.green@gmail.com>
2015-10-08 15:12:58 -0600

    fixes config/routes

--- chore_hero/config/routes.rb
+++ chore_hero/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
 
-  get 'static_pages/faq'
+  get '/faq', to: 'static_pages#faq', as: 'faq'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/home', to: 'static_pages#home', as: 'home'
 
-  root 'static_pages#home'
+  root to: 'static_pages#home'
   
   resources :badges
   resources :chores
