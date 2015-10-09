b7b73e3afc497045002756401d6cf401d8c3601b
Scotchard22 <scotchard@aol.com>
2015-10-09 15:20:29 -0600

    chore testing

--- chore_hero/spec/controllers/chores_controller_spec.rb
+++ chore_hero/spec/controllers/chores_controller_spec.rb
@@ -44,6 +44,12 @@ RSpec.describe ChoresController, type: :controller do
       chore = chore.reload
       expect(chore.title).to eq('dont clean')
     end
+    # it 'renders edit template' do
+    #   chore = Chore.create(title: 'clean', description: 'clean dishes', xp_value: 5.5)
+    #   update_params = {id: chore.id, chore:{title: nil}}
+    #   put :update, update_params
+    #   expect(response).to render_template('edit')
+    # end  
   end    
 
   describe "GET #show" do
@@ -57,6 +63,13 @@ RSpec.describe ChoresController, type: :controller do
     end
   end
 
+  describe "DELETE #destroy" do
+    it 'deletes chore' do 
+      chore = Chore.create(title: 'Pick up crayons')
+      delete :destroy, {id: chore.id}
+      expect(Chore.count).to eq(0)
+    end
+  end    
 
 
 end
