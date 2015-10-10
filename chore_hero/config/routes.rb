1cfc2b9a049f8bcf908114bf0eae3ba65d55d401
BraydenGreen <brayden.p.green@gmail.com>
2015-10-09 22:51:35 -0600

    WIP: trying to fix Parent sign out path

--- chore_hero/config/routes.rb
+++ chore_hero/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :children, controllers: { sessions: 'children/sessions'}
   devise_for :parents, controllers: {sessions: 'parent/sessions', registrations: 'parent/registrations'}
+  
   resources :badges
   resources :chores
 
