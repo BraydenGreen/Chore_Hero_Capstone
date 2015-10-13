c930b63a1fc8171c0be18ee305b62b42c6297589
derrekrb <derrek.ray@gmail.comcom>
2015-10-13 17:21:10 -0600

    all the good stuff

--- chore_hero/app/controllers/children_controller.rb
+++ chore_hero/app/controllers/children_controller.rb
@@ -1,6 +1,51 @@
 class ChildrenController < ApplicationController
+  before_filter :authenticate_parent!, except: [:index, :show]
+  before_action :children, only: [:show, :edit, :update, :destroy]
+
   def index
+    @child = Child.all
   end
 
-  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @child.update(children_params)
+      redirect_to children_path(@child.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def new
+    @child = Child.new
+  end
+
+  def create
+    @child = Child.new(children_params)
+      if @child.save
+        redirect_to children_path(@child.id)
+      else
+        render 'new'
+      end
+  end
+
+  def destroy
+    @child.destroy
+    redirect_to children_path
+  end
+
+  private
+
+     def children_params
+       params.require(:child).permit(:title, :description, :xp_value, :complete)
+     end
+
+     def children
+       @child = Child.find(params[:id])
+     end
+
 end
--- chore_hero/app/models/child.rb
+++ chore_hero/app/models/child.rb
@@ -1,11 +1,11 @@
 class Child < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable :registerable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :chores
   has_one :profile
   has_many :badges
-  belongs_to :parent       
+  belongs_to :parent
 end
