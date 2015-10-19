e28c1602dab452c4ebe723ab7a3558f3e4716511
wulfman97 <calebschade97@gmail.com>
2015-10-19 15:22:33 -0600

    working on paperclip

--- chore_hero/app/controllers/application_controller.rb
+++ chore_hero/app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   #before_action :authenticate_user!
   # before_action :configure_permitted_parameters, if: :devise_controller?
   # protected
@@ -16,5 +18,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
+    end
 
 end
--- chore_hero/app/models/user.rb
+++ chore_hero/app/models/user.rb
@@ -2,6 +2,13 @@ class User < ActiveRecord::Base
   has_many :children
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "60x60" }, :default_url => "/assets/images/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
