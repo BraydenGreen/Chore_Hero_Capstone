4da73c73187c94340d50e47817edfeb5559043fb
derrekrb <derrek.ray@gmail.comcom>
2015-10-19 14:52:32 -0600

    styling 2

--- chore_hero/config/routes.rb
+++ chore_hero/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
-    
+
   resources :children
   resources :badges
   resources :chores
 
   get '/faq', to: 'static_pages#faq', as: 'faq'
   get '/about', to: 'static_pages#about', as: 'about'
-  
 
-  resources :users, :only => [:show, :edit, :update, :destroy]
+
+  # resources :users, :only => [:show, :edit, :update, :destroy]
 end
