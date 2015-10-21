8c6dd7e22853690c1acf9304d9714bfc307ae40d
wulfman97 <calebschade97@gmail.com>
2015-10-21 09:38:41 -0600

    working twitter api

--- chore_hero/app/controllers/static_pages_controller.rb
+++ chore_hero/app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
   
   def home
+    @twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['consumer_key']
+      config.consumer_secret = ENV['consumer_secret']
+      config.access_token = ENV['access_token']
+      config.access_token_secret = ENV['access_token_secret']
+    end
   end
 
   def about
