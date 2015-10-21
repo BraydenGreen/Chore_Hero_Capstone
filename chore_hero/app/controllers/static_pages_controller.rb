a40d79fc5243b6806be3d036469d2b7fa50a3a82
wulfman97 <calebschade97@gmail.com>
2015-10-21 14:15:07 -0600

    working twitter api on about page

--- chore_hero/app/controllers/static_pages_controller.rb
+++ chore_hero/app/controllers/static_pages_controller.rb
@@ -1,15 +1,15 @@
 class StaticPagesController < ApplicationController
   
   def home
+  end
+
+  def about
     @twitter = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['consumer_key']
       config.consumer_secret = ENV['consumer_secret']
       config.access_token = ENV['access_token']
       config.access_token_secret = ENV['access_token_secret']
     end
-  end
-
-  def about
   end
 
   def faq
