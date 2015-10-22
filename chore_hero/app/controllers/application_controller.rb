573b0b1b874800b6540476c0512eb879ce2f4deb
wulfman97 <calebschade97@gmail.com>
2015-10-21 20:47:40 -0600

    add avatar on sign up and fix twitter api

--- chore_hero/app/controllers/application_controller.rb
+++ chore_hero/app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :type) }
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :type, :avatar) }
         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
     end
 
