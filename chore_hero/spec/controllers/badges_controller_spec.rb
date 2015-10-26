9d169ef8d91d90e04ea26729e2ea7fa1d59de41a
wulfman97 <calebschade97@gmail.com>
2015-10-25 22:25:16 -0600

    self adjusting fixed footer

--- chore_hero/spec/controllers/badges_controller_spec.rb
+++ chore_hero/spec/controllers/badges_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 RSpec.describe BadgesController, type: :controller do
 
   describe "GET #index" do
@@ -9,17 +10,30 @@ RSpec.describe BadgesController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe "POST #create" do
+    it "creates a new badge" do
+      badge = Badge.create(title:'Good')
+      # expect(Badge.count).to eq(0)
+      expect(Badge.count).to eq(1)
+      expect(Badge.last.title).to eq('Good')
+      
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "PUT #update" do 
+    it 'updates a badge' do 
+      badge = Badge.create(title: 'clean')
+      update_params = {id: badge.id, badge:{title: 'dont clean'}}
+      put :update, update_params
+      badge = badge.reload
+      expect(badge.title).to eq('dont clean')
     end
   end
 
--- chore_hero/spec/rails_helper.rb
+++ chore_hero/spec/rails_helper.rb
@@ -1,4 +1,5 @@
 require 'factory_girl_rails'
+require 'devise'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -29,9 +30,11 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include Devise::TestHelpers, :type => :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
