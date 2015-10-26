1748e00372043d7cd027f0803645b468f2f109de
wulfman97 <calebschade97@gmail.com>
2015-10-26 16:39:44 -0600

    remove dotenv

--- chore_hero/config/application.rb
+++ chore_hero/config/application.rb
@@ -15,7 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 
 HOSTNAME = ENV['HOSTNAME']
 
